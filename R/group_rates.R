015b0e589374fe86a91a9152a65ed3e01caa3c8b
Rafael A. Irizarry <rairizarry@gmail.com>
2021-05-10 10:16:16 -0400

    adding group_rates

--- R/group_rates.R
+++ R/group_rates.R
@@ -1,4 +1,4 @@
-group_means <- function(y, g){
+group_rates <- function(y, g){
 
   if(!is.factor(g)){
     warning("Coercing g into a factor")
@@ -7,19 +7,23 @@ group_means <- function(y, g){
 
   js <- as.numeric(g)
 
-  n <- sapply(split(seq_along(g), g), length)
-
-  m <- matrix(0, nrow(y),  length(n))
+  rowsums <- matrix(0, nrow(y),  length(n))
+  colsums <- vector("numeric", length(n))
 
   for(j in 1:ncol(y)){
     ind <- (y@p[j]+1):y@p[j+1]
     real_ind <- y@i[ind] + 1
     k <- js[j]
-    m[real_ind, k] <- m[real_ind, k] + y@x[ind]/n[k]
+    x <- y@x[ind]
+    rowsums[real_ind, k] <- rowsums[real_ind, k] + x
+    colsums[k] <- colsums[k] + sum(x)
   }
-  colnames(m) <- levels(g)
-  rownames(m) <- rownames(y)
 
-  return(m)
+  rowsums <- sweep(rowsums, 2, colsums, FUN = "/")
+  colnames(rowsums) <- levels(g)
+  rownames(rowsums) <- rownames(y)
+
+  return(rowsums)
+
 }
 
