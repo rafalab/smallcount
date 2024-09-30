342a9d39ccd0b774f6f8a2ac5a012585f674e20b
ggrajeda <gabriel.grajeda@gmail.com>
2024-09-30 16:43:59 -0400

    Create abstraction for matrix transformations
    Currently, the transformation functions must have zero as a fixed point. Classes will be expanded to include more general functions in upcoming commits.

---
+++ R/classes.R
@@ -0,0 +1,128 @@
+setClassUnion("numericOrNull", c("numeric", "NULL"))
+setClassUnion("functionOrNull", c("function", "NULL"))
+
+#' Preprocessing on Sparse Count Matrices
+#'
+#' @slot func Transformation to be applied to sparse count matrix. Should be a
+#'   function with a single parameter.
+#' @slot center_rows Whether transformed rows should be shifted to have mean
+#'   zero.
+#' @slot center_cols Whether transformed columns should be shifted to have mean
+#'   zero.
+#' @slot scale Whether transformed rows should be scaled to have unit variance.
+#'
+#' @export
+setClass(
+  "CountTransform",
+  slots = c(
+    func = "functionOrNull",
+    center_rows = "logical",
+    center_cols = "logical",
+    scale = "logical"
+  )
+)
+
+# Helper function so that users can specify a single logical value for whether
+# the rows of the data should be centered.
+get_duplicated_argument <- function(arg, name) {
+  if (length(arg == 1)) c(arg, FALSE) else arg
+}
+
+#' Preprocessing on Sparse Count Matrices
+#'
+#' This class is an abstraction for the preprocessing transformations that are
+#' applied to count data before statistical methods like PCA (e.g., log1p,
+#' scaling, row-centering). Note that these transformations may not preserve the
+#' sparsity of the matrix.
+#'
+#' @param func Transformation to be applied to sparse count matrix. Should be a
+#'   function with a single parameter.
+#' @param center Whether transformed data should be shifted to have mean zero.
+#'   Can be specified either as a vector of two logical values, specifying
+#'   whether the rows/columns should be centered, respectively, or a single
+#'   logical value specifying whether the columns should be centered (for
+#'   consistency with [stats::prcomp()]).
+#' @param scale Whether transformed rows should be scaled to have unit variance.
+#'
+#' @return CountTransform object
+#' @export
+#'
+#' @examples
+#' triple <- CountTransform(function(x) 3 * x, center = FALSE, scale = FALSE)
+CountTransform <- function(func, center = FALSE, scale = FALSE) {
+  center <- get_duplicated_argument(center, "center")
+  new("CountTransform", func = func, center_rows = center[1],
+      center_cols = center[2], scale = scale)
+}
+
+#' Transformed Count Matrix
+#'
+#' @slot y SparseMatrix object.
+#' @slot row_offset,col_offset Vectors whose product
+#'   `outer(row_offset, col_offset)` represents the residual between `y` and a
+#'   dense transformation of `y` (e.g., row-centered `y`).
+#'
+#' @export
+setClass(
+  "TransformedMatrix",
+  slots = c(
+    y = "SparseMatrix",
+    row_offset = "numericOrNull",
+    col_offset = "numericOrNull"
+  )
+)
+
+#' Transformed Count Matrix
+#'
+#' @param y SparseMatrix object.
+#' @param transform Transformation to apply to `y`.
+#'
+#' @return TransformedMatrix object.
+#' @export
+#'
+#' @examples
+#' mat <- as(matrix(c(1:9), nrow=3, ncol=3), "SVT_SparseMatrix")
+#' triple <- CountTransform(function(x) 3 * x, center = FALSE, scale = FALSE)
+#' tripled_mat <- TransformedMatrix(mat, triple)
+TransformedMatrix <- function(y, transform) {
+  # Apply the transformation.
+  if (!is.null(transform@func)) {
+    y[nzwhich(y)] <- transform@func(y[nzwhich(y)])
+  }
+
+  # Scale the rows if requested.
+  if (transform@scale) {
+    # Calculate the standard deviations of the rows.
+    sds <- sqrt((rowSums(y^2) - rowSums(y)^2 / ncol(y)) / (ncol(y) - 1))
+    nz_ind <- nzwhich(y, arr.ind = TRUE)
+    y[nz_ind] <- y[nz_ind] / sds[nz_ind[, 1]]
+  }
+
+  # Store the row/column centers if requested.
+  col_offset <- NULL
+  row_offset <- NULL
+  if (transform@center_rows && transform@center_cols) {
+    col_offset <- colSums(y)
+    row_offset <- rowSums(y) / sum(col_offset)
+  } else if (transform@center_rows) {
+    col_offset <- rep(1 / ncol(y), ncol(y))
+    row_offset <- rowSums(y)
+  } else if (transform@center_cols) {
+    col_offset <- colSums(y)
+    row_offset <- rep(1 / nrow(y), nrow(y))
+  }
+  new("TransformedMatrix", y = y, row_offset = row_offset,
+      col_offset = col_offset)
+}
+
+setMethod("as.matrix", "TransformedMatrix", function(x, ...) {
+  if (is.null(x@row_offset) || is.null(x@row_offset)) {
+    as.matrix(x@y)
+  } else {
+    as.matrix(x@y) - outer(x@row_offset, x@col_offset)
+  }
+})
+
+setMethod("as.array", "TransformedMatrix", function(x, ...) {
+  as.array(as.matrix(x))
+})
---
+++ R/transforms.R
@@ -0,0 +1,55 @@
+#' Identity Transformation
+#'
+#' Maps x to itself.
+#'
+#' @note Internally, the identity function is represented by a NULL value.
+#'
+#' @param center Whether transformed data should be shifted to have mean zero.
+#'   Can be specified either as a vector of two logical values, specifying
+#'   whether the rows/columns should be centered, respectively, or a single
+#'   logical value specifying whether the columns should be centered (for
+#'   consistency with [stats::prcomp()]).
+#' @param scale Whether transformed rows should be scaled to have unit variance.
+#'
+#' @return CountTransform object.
+#' @export
+identity_transform <- function(center = FALSE, scale = FALSE) {
+  CountTransform(NULL, center, scale)
+}
+
+#' Log1p Transformation
+#'
+#' Maps x to log(x + 1).
+#'
+#' @inheritParams identity_transform
+#'
+#' @return CountTransform object.
+#' @export
+log1p_transform <- function(center = FALSE, scale = FALSE) {
+  CountTransform(log1p, center, scale)
+}
+
+#' Scaled Log1p Transformation
+#'
+#' Maps x to log(`coef` * x + 1).
+#'
+#' @param coef Scaling coefficient.
+#' @inheritParams identity_transform
+#'
+#' @return CountTransform object.
+#' @export
+scaled_log1p_transform <- function(coef, center = FALSE, scale = FALSE) {
+  CountTransform(function(y) log1p(coef * y), center, scale)
+}
+
+#' CPM Log1p Transformation
+#'
+#' Maps x to log(x/1000000 + 1).
+#'
+#' @inheritParams identity_transform
+#'
+#' @return CountTransform object.
+#' @export
+cpm_log1p_transform <- function(center = FALSE, scale = FALSE) {
+  CountTransform(function(y) log1p(y / 1e6), center, scale)
+}
